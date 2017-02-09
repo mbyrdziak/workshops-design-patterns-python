d11a9438c81ff0d7dd18026a724bdb1bea7041b0
Marcin Byrdziak <marcin.byrdziak@pragmaticcoders.com>
2017-02-09 11:06:09 +0100

    Structure change

--- csv-reader/ParticipantRepository.py
+++ csv-reader/ParticipantRepository.py
@@ -4,6 +4,6 @@ from Participant import Participant
 class ParticipantRepository:
     def save(self, participant: Participant):
         if not type(participant) is Participant:
-            raise Exception("Incorrect class " + str(
-                type(participant)) + " should be " + str(Participant))
+            raise Exception("Incorrect class {} should be {}".format(
+                str(type(participant)), str(Participant)))
         print("Persisting participant")
