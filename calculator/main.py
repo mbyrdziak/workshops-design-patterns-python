670c3e7272ad8733548b848204851884c9d5e7b9
Marcin Byrdziak <marcin.byrdziak@pragmaticcoders.com>
2018-02-11 11:23:50 +0100

    Add calculator hints

--- calculator/main.py
+++ calculator/main.py
@@ -5,9 +5,13 @@ def main():
     calculator = Calculator()
 
     calculator.execute()  # implement add 100
+    print(calculator.currentValue())  # should show 100
     calculator.execute()  # implement subtract 50
+    print(calculator.currentValue())  # should show 50
     calculator.execute()  # implement multiply by 4
+    print(calculator.currentValue())  # should show 200
     calculator.execute()  # implement divide by 2
+    print(calculator.currentValue())  # should show 100
 
     calculator.undo()
     calculator.undo()
